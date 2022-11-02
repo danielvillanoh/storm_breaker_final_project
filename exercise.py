840cc750ce45413bc60f1a8fe4d81b5285ee0c77
Daniel Villano-Herrera <danielvillanoh01@gmail.com>
2022-11-01 21:02:00 -0400

    Added a Python file called exercise.py

---
+++ exercise.py
@@ -0,0 +1,18 @@
+""" This is a random test. Feel free add classes, functions, or anything of 
+that nature. 
+
+"""
+
+class Triangle:
+    """ Creating a Triangle class where we determine the triangle     
+    Attributes:
+        s1 (float): First side of the triangle 
+        s2 (float): Second side of the triangle 
+        s3 (float): Third side of the triangle  
+    """
+    def __init__(self,s1,s2,s3):
+        self.s1 = s1
+        self.s2 = s2
+        self.s3 = s3
+   
+        
