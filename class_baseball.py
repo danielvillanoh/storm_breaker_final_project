9176478617d1e2ec762a01390ae30eeb47d195e9
Daniel Villano-Herrera <danielvillanoh01@gmail.com>
2022-11-18 22:01:00 -0500

    Removed optional parameters in Player class.

--- class_baseball.py
+++ class_baseball.py
@@ -26,19 +26,17 @@ class Player():
         
         We will also the add 'score' attribute in the init magic method. 
         """
-    def swing(self, pitch_and_catch = False):
+    def swing(self):
         """ The player swings the ball.
-        Argument:
-            pitch_and_catch(boolean): This will determine on whether the player
-            is pitching and catching the ball, false by default. 
         """
 
-    def pitch(self,pitch_and_catch = True):
+    def pitch(self):
         """ The player pitches the ball. Only possible if the player isn't 
         swinging. 
         
         Arguement: 
-
+            pitch_and_catch(boolean): This will determine on whether the player
+            is pitching and catching the, 
         
         """
         
@@ -54,6 +52,8 @@ class Player():
 class Opponent(Player):
     """ This class represents the opponent or the second player. 
     The opponent class will inherit the properites of the Player class.  
+    
+    
     """
 
 class Baseball:
